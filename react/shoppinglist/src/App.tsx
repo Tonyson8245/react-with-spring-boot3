28350bd0ca5cd01f8d3f30d24bdac3491469d06a
Tonyson8245 <dongo8245@gmail.com>
2025-02-12 21:01:58 +0900

    Add material-ui shopping List

--- react/shoppinglist/src/App.tsx
+++ react/shoppinglist/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Container from "@mui/material/Container";
+import { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+
+import "./App.css";
+import AddItem from "./AddItem";
+export type Item = {
+  product: string;
+  amount: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState<Item[]>([]);
+
+  const addItem = (item: Item) => {
+    setItems([...items, item]);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Shopping List</Typography>
+          </Toolbar>
+        </AppBar>
+        <AddItem addItem={addItem} />
+        <List>
+          {items.map((item, index) => (
+            <ListItem key={index} divider>
+              <ListItemText primary={item.product} secondary={item.amount} />
+            </ListItem>
+          ))}
+        </List>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
