a2187c2375b9629d027ff6a6a288513254db17f4
Tonyson8245 <dongo8245@gmail.com>
2025-02-13 18:44:39 +0900

    migration table to x-data-grid

--- react/carfront/src/components/Carlist.tsx
+++ react/carfront/src/components/Carlist.tsx
@@ -2,6 +2,7 @@ import { getCars } from "../api/carapi";
 import { CarResponse } from "../types";
 
 import { useQuery } from "@tanstack/react-query";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 function Carlist() {
   const { data, error, isSuccess } = useQuery<CarResponse[]>({
@@ -9,26 +10,34 @@ function Carlist() {
     queryFn: getCars,
   });
 
+  const columns: GridColDef[] = [
+    {
+      field: "brand",
+      headerName: "Brand",
+      width: 200,
+    },
+    { field: "model", headerName: "Model", width: 200 },
+    { field: "color", headerName: "Color", width: 200 },
+    {
+      field: "registerNumber",
+      headerName: "Reg.nr",
+      width: 150,
+    },
+    { field: "modelYear", headerName: "Model Year", width: 150 },
+    { field: "price", headerName: "Price", width: 150 },
+  ];
+
   if (!isSuccess) {
     return <div>Loading...</div>;
   } else if (error) {
     return <div>Error when fetching cars...</div>;
   } else {
     return (
-      <table>
-        <tbody>
-          {data.map((car: CarResponse) => (
-            <tr key={car._links.self.href}>
-              <td>{car.brand}</td>
-              <td>{car.model}</td>
-              <td>{car.color}</td>
-              <td>{car.registrationNumber}</td>
-              <td>{car.modelYear}</td>
-              <td>{car.price}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        getRowId={(row) => row._links.self.href}
+      />
     );
   }
 }
